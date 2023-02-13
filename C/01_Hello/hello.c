83c0208a9728bfd46815bec4526630a4cc1fadf6
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-02-13 11:50:38 +0300

    first c exercise

--- C/01_Hello/hello.c
+++ C/01_Hello/hello.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello Everyone!");
+    // printf("Hello Everyone!");
+
+/*
+    FORMAT BELİRLEYİCİLER
+    %d : int
+    %c : char3
+    %f : float
+*/
+
+/* aritmetik işlemler */
+    printf("%d\n", 10*4);
+    printf("%d\n", 10/4);
+    printf("%d\n", 10+4);
+    printf("%d\n", 10-4);
+
+
+    int x;
+    printf("Bir sayi giriniz:");
+    scanf("%d",&x); //kullanıcıdan input alma
+    printf("Girdiginiz sayi:%d\n",x); //kullanıcıdan alınan inputu yazdırma
+
+
+
 }
