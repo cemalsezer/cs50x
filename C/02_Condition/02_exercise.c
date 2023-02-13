852e35fab182d1bf2395ce37a602d8aa3fff6fc3
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-02-13 11:59:12 +0300

    condition is done

--- C/02_Condition/02_exercise.c
+++ C/02_Condition/02_exercise.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main (void){
+
+    int hiz = 190;
+
+    if (hiz>100)
+    {
+        printf("Hiz limitini astin\n");
+    }
+    else{
+        printf("Hiz limitini asmadin");
+    }
     
+
 }
