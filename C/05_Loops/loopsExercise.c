220166029027cf67d78f534cec84473cbc8dd0be
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-02-14 16:53:29 +0300

    loops exercise is build

---
+++ C/05_Loops/loopsExercise.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+        int rows,columns;
+        char symbol;
+
+        printf("Satir sayisini giriniz: ");
+        scanf("%d", &rows);
+        printf("Sutun sayisini giriniz: ");
+        scanf("%d", &columns);
+        scanf("%c");
+        printf("Satir sayisini giriniz: ");
+        scanf("%c", &symbol);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                printf("%c",symbol);
+            }
+            printf("\n");
+        }
+    
+}
