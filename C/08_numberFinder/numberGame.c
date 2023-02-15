2e0476e54a11b38a25ff93aca13d51829e120176
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-02-15 11:20:38 +0300

    game is done

---
+++ C/08_numberFinder/numberGame.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+    int max=10;
+    int min=1;
+    int trueNumber;
+    int guess;
+    int numberOfGuess;
+
+    srand(time(0));
+
+    trueNumber=(rand()%max)+min;
+
+    do
+    {
+        printf("Bir sayi giriniz: ");
+         scanf("%d",guess);
+         if(guess>trueNumber){
+            printf("Asagi in");
+         }
+            else if(guess<trueNumber){
+             printf("Yukari cik");
+            }
+            else{
+            printf("Bildin \n");  
+            }
+            numberOfGuess++;
+    } while (guess != trueNumber);
+    
+    printf("Dogru cevap: %d\n",trueNumber);
+    printf("%d denemde buldun",numberOfGuess);
+    
+
+    return 0;
+}
