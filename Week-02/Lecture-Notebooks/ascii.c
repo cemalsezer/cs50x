cc9596bcbf27e4b22f14041274a619cf6eb4e5d9
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-06-01 18:24:35 +0300

    ascii is added

---
+++ Week-02/Lecture-Notebooks/ascii.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// #include <cs50.h>
+// #include <string.h>
+
+// int main(void){
+//     string s =get_string("Enter Text: ");
+//     for(int i=0; i<strlen(s); i++){
+        
+//         int c = (int) s[i];
+//         printf("%c %d\n",s[i],c);
+//     }
+// }
