b67fc75c98d1f4e75036d346572430142d92ee73
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-06-01 15:55:35 +0300

    lecture note is added

---
+++ Week-02/Lecture-Notebooks/lecture1.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// #include <cs50.h>
+
+void chart(int scr){
+        for(int i=1;i<=scr;i++){
+         printf("#");
+        }
+        printf("\n");
+    }
+
+int main(void){
+    int scores[3];
+    for(int i = 0;i<3;i++){
+        scores[i] = get_int("Score %i: ",i+1);
+    }
+    
+    for(int i = 0;i<3;i++){
+        printf("Scores %i: ",i+1);
+        chart(scores[i]);
+    }
+   
+
+}
+
