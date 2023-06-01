9dc09f7daf19c8f21476136373c16d5052822b9f
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-06-01 17:57:50 +0300

    lecture2 is added

---
+++ Week-02/Lecture-Notebooks/lecture2.c
@@ -0,0 +1,39 @@
+// #include <stdio.h>
+// #include <cs50.h>
+
+// void chart(int count, int scores[]){
+    
+//     for (int i=0;i<count;i++){
+
+//         for(int j=0;j<scores[count];j++){
+//             printf("#");
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main(void){
+//     int count=get_int("How many: ");
+//     int scores[count];
+
+//     for(int i = 0;i<count;i++){
+        
+//         scores[i] = get_int("Enter Score %i: ",i+1);
+//         // printf("S: %d",scores[i]);
+//         for(int j=0;j<scores[i];j++){
+//             printf("#");
+//         }
+//         printf("\n");
+//     }
+
+    // for(int j=0;j<count;j++){
+    //     printf("%d",scores[j]);
+    //     printf("\n");
+    // }
+    
+    
+    // for(int i = 0;i<count; i++){
+    //     printf("Scores %d: ",i+1);
+    //     chart(count,scores[i]);
+    // }
+// }
