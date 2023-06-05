4be30aaa7b3602bd380d66a4f24e4ad4348ac528
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-06-05 14:22:18 +0300

    linear_search exercise is added

--- Week-02/Lecture-Notebooks/linear_search.c
+++ Week-02/Lecture-Notebooks/linear_search.c
@@ -1,43 +1,37 @@
-// #include <stdio.h>
+#include <stdio.h>
 // #include <cs50.h>
 
-int linearSearch(int arraysNums[],int arraysDims,int searchNum){
-    int i;
-    for(i=0;i<arraysDims;i++){
-        if(arraysNums[i]==searchNum)
+int  linearSearch(int arrays[], int arrSize, int searchNr){
+    for(int i =0; i<arrSize;i++){
+        if(arrays[i]==searchNr){
             return 1;
+        }
     }
-        return -1;
+    return -1;
 }
 
 
-
-
 int main(void){
-
-    int arraysDim;
-
-    printf("Enter dimmension of array: ");
-    scanf("%d",&arraysDim);
-
-    int arraysNum[arraysDim];
+    int arraySize;
+    printf("Enter the array size: ");
+    scanf("%d",&arraySize);
+    int arrayNums[arraySize];
     int i=0;
-    
-    while(i<arraysDim){
-       printf("Enter numbers of array: ");
-       scanf("%d",&arraysNum[i]);
-       i++;
-   }
     int searchNum;
-    printf("Enter the number of searching: ");
+    while(i<arraySize){
+        printf("Enter nums: ");
+        scanf("%d",&arrayNums[i]);
+        i++;
+    }
+    printf("Enter the to search num: ");
     scanf("%d",&searchNum);
-    int result=linearSearch(arraysNum,arraysDim,searchNum);
-    if(result==-1){
-        printf("Not found!");
+
+   int result = linearSearch(arrayNums,arraySize,searchNum);
+    if(result==1){
+        printf("Found");
     }
     else{
-        printf("Founded!");
+        printf("Not found");
     }
-    
 
 }
