0e21ec1492b2fb038d4daf3a058e0c11cc0d8747
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-06-06 18:15:38 +0300

    algorithm is done

--- Week-02/Lecture-Notebooks/merge_sort.c
+++ Week-02/Lecture-Notebooks/merge_sort.c
@@ -1,2 +1,46 @@
 #include <stdio.h>
 
+int a[]={8,7,6,5,4,3,2,1};
+int b[8];
+
+void merge(int start, int mid, int end){
+    int i,j,k;
+    for(i=start;i<=end;i++){
+        i=start;
+        j=mid+1;
+        k=start;
+    }
+    while (i<=mid && j<=end)
+    {
+        if(b[i]<=b[j]){
+            a[k++]=b[i++];
+        }
+        else{
+            a[k++]=b[j++];
+        }
+    }
+    while (i<=mid)
+    {
+        a[k++]=b[i++];
+    }
+ 
+}
+    
+void divide( int start, int end){
+    if(start<end){
+         
+        int mid=(start+end)/2;
+        divide(start, mid);
+        divide(mid+1, end);
+
+        merge(start, mid, end);
+    }
+}
+
+int main(){
+    divide(0,8);
+    for(int i=0; i<=7; i++)
+        printf("%d", a[i]);
+
+        return 0;
+}
--- Week-02/Lecture-Notebooks/tempCodeRunnerFile.c
+++ Week-02/Lecture-Notebooks/tempCodeRunnerFile.c
@@ -1,6 +1,37 @@
-swap(int *j,int *i){
-//     int temp;
-//     temp=*j;
-//     *j=*i;
-//     *i=temp;
-// }
+#include <stdio.h>
+// #include <cs50.h>
+
+int  linearSearch(int arrays[], int arrSize, int searchNr){
+    for(int i =0; i<arrSize;i++){
+        if(arrays[i]==searchNr){
+            return 1;
+        }
+    }
+    return -1;
+}
+
+
+int main(void){
+    int arraySize;
+    printf("Enter the array size: ");
+    scanf("%d",&arraySize);
+    int arrayNums[arraySize];
+    int i=0;
+    int searchNum;
+    while(i<arraySize){
+        printf("Enter nums: ");
+        scanf("%d",&arrayNums[i]);
+        i++;
+    }
+    printf("Enter the to search num: ");
+    scanf("%d",&searchNum);
+
+   int result = linearSearch(arrayNums,arraySize,searchNum);
+    if(result==1){
+        printf("Found");
+    }
+    else{
+        printf("Not found");
+    }
+
+}
