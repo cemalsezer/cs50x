abfcb852c1f664d814c26840cabae93f5295fb87
Cemal Sezer <cemalsezer.dev@gmail.com>
2023-06-14 14:23:04 +0300

    caesar pset is done

--- Week-02/Problem-Set2/caesar.c
+++ Week-02/Problem-Set2/caesar.c
@@ -1,24 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <cs50.h>
 
-int main(void){
-    char text[100];
-    int key;
-    printf("Enter the text to be encrypted: ");
-    scanf("%[^\n]s", text);
-    printf("Enter the key: ");
-    scanf("%d", &key);
-    for(int i = 0; i < strlen(text); i++){
-        if(isalpha(text[i])){
-            if(isupper(text[i])){
-                text[i] = ((text[i] - 'A' + key) % 26) + 'A';
+int main(int argc, string argv[]){
+    if (argc!=2){
+        printf ("Usage: ./caesar key\n");
+        return 1;
+    }
+    int sayib = atoi(argv[1]);
+    int alib = strlen(argv[1]);
+    
+    if(sayib==0){
+        printf ("Usage: ./caesar key\n");
+        return 0;
+    }
+    if (sayib<alib){
+        printf("Usage: ./caesar key\n");
+        return 0;
+    }
+    
+    int key = atoi(argv[1]);
+    if(key>=26){
+        key = key%26;
+    }
+    string text = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+
+    for(int i=0, n=strlen(text);i<n;i++){
+
+        if(text[i]>='a'&&text[i]<='z'){
+            int kucuk = 'z'-text[i];
+            printf("\n kucuk: %d",kucuk);
+            printf("\n key: %d",key);
+
+            if(key>kucuk){
+                int k=(key-kucuk)-1;
+                printf("\n k: %d",k);
+                text[i]='a';
+                printf("%c",text[i]+k);
+                continue;
+        }
+             printf("%c",text[i]+key);
+             continue;
+        }
+        else if(text[i]>='A'&&text[i]<='Z'){
+
+            int buyuk = 'Z'-text[i];
+            if(key>buyuk){
+                int b=(key-buyuk)-1;
+                text[i]='A';
+                printf("%c",text[i]+b);
+                continue;
             }
-            else{
-                text[i] = ((text[i] - 'a' + key) % 26) + 'a';
-            }
+        printf("%c",text[i]+key);
+    }
+        else{
+            printf("%c",text[i]);
         }
     }
-    printf("Encrypted text: %s\n", text);
-    return 0;
+    printf("\n");
+    
 }
